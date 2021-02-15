ee809f3679470019cac96c89a6c0311f9cc89fce
florianmachin <tiflo99@gmail.com>
2021-02-15 17:36:50 +0100

    import export coremodule

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,9 +5,11 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
+  declarations: [HeaderComponent, FooterComponent],
   imports: [
     CommonModule
   ],
-  declarations: [HeaderComponent, FooterComponent]
+  exports: [HeaderComponent, FooterComponent]
+
 })
 export class CoreModule { }
