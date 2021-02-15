137fa0a90655ab405d2801e35bb17afbc57c81aa
florianmachin <tiflo99@gmail.com>
2021-02-15 17:28:52 +0100

    Footer-Header

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CoreComponent } from './core.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [CoreComponent]
+  declarations: [HeaderComponent, FooterComponent]
 })
 export class CoreModule { }
